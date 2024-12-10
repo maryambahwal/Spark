257e8662ceda0e765b6147789c42455416846cc1
Maryam Bahwal <mrim2m7md@gmail.com>
2024-12-10 21:16:12 +0300

    Initial Commit

---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Maryam Bahwal on 09/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
