2bbb0f7a760d4f7853a78658ccac8fbf1d7fe0e1
present me app <Ghadasub@outlook.com>
2024-12-16 10:32:09 +0300

    ghada work

---
+++ HomePsge.swift
@@ -0,0 +1,17 @@
+//
+//  HomePsge.swift
+//  SparkOnB
+//
+//  Created by Ghada Alsubaie on 10/06/1446 AH.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        VStack {
+          
+        }
+        .navigationBarHidden(true) // Hide the navigation bar on HomePage if you want
+    }
+}
